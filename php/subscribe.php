82dbb38d6828229a32e8ec7092e1d217c8aeba24
Fernandez <fernareb@186590d351eb.ant.amazon.com>
2018-06-05 09:35:52 -0700

    first commit

---
+++ php/subscribe.php
@@ -0,0 +1,29 @@
+<?php
+
+	// MailChimp
+	$APIKey = '0bac1dc72a435d986316c5b79a48a6f2-us15';
+	$listID = '7295b8a29c';
+
+	$email   = $_POST['email'];
+
+	require_once('inc/MCAPI.class.php');
+
+	$api = new MCAPI($APIKey);
+	$list_id = $listID;
+
+	if($api->listSubscribe($list_id, $email) === true) {
+		$sendstatus = 1;
+		$message = '<div class="alert alert-success subscription-success" role="alert"><strong>Success!</strong> Check your email to confirm sign up.</div>';
+	} else {
+		$sendstatus = 0;
+		$message = '<div class="alert alert-danger subscription-error" role="alert"><strong>Error:</strong> ' . $api->errorMessage.'</div>';
+	}
+
+	$result = array(
+		'sendstatus' => $sendstatus,
+		'message' => $message
+	);
+
+	echo json_encode($result);
+
+?>
